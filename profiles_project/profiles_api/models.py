5d31988e4170ff75eabc83126ae1bcb4d0c6164e
uzzal71 <uzzalroy.acm@gmail.com>
2019-01-12 19:11:36 +0600

    add post method

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -3,19 +3,18 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
 
 class UserProfileManager(BaseUserManager):
-    """ Helps django work with our custom user model """
+    """Helps Django work with our custom user model."""
 
     def create_user(self, email, name, password=None):
-        """ Creates a new user profile """
+        """Creates a new user profile."""
 
         if not email:
-            raise ValueError("User must have an email address.")
+            raise ValueError('Users must have an email address.')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -27,15 +26,15 @@ class UserProfileManager(BaseUserManager):
 
         user = self.create_user(email, name, password)
 
-        user.is_active = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
 
         return user
 
-
+        
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """ 
+    """
     Represents a "user profile" inside out system. Stores all user account
     related data, such as 'email address' and 'name'.
     """
@@ -43,7 +42,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
@@ -51,11 +50,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
-        """ Django use this when it needs to get the user's full name """
+        """Django uses this when it needs to get the user's full name."""
 
         return self.name
 
     def get_short_name(self):
-        """ Django use this when it needs to get the user's short name """
+        """Django uses this when it needs to get the users abbreviated name."""
 
-        return self.name    
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to text."""
+
+        return self.email
--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .import serializers
 
 # Create your views here.
+class HelloAPIView(APIView):
+    ''' Test API View '''
+    
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        ''' Returns a list of API features'''
+
+        an_apiview= [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """ create a hello message with our name """
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
