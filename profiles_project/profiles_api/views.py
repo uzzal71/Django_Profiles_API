626973eb90fe6cc1469fbb7bb0789d5e1603c4bc
uzzal71 <uzzalroy.acm@gmail.com>
2019-01-15 21:58:18 +0600

    Add this module  IsAuthenticatedOrReadOnly views.py

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -11,6 +11,7 @@ from . import permissions
 from rest_framework import filters
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 class HelloAPIView(APIView):
