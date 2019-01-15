7e45d8145d9fa588658524b867c2657166134ba5
uzzal71 <uzzalroy.acm@gmail.com>
2019-01-15 21:46:57 +0600

    Create ProfileFeedAPI Serializer

--- profiles_project/profiles_api/serializers.py
+++ profiles_project/profiles_api/serializers.py
@@ -26,3 +26,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         user.save()
 
         return user    
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """A serializer for profile feed items."""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'create_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}        
