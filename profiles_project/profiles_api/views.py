865efd803136f1cc220518222c7562f1dc90f430
uzzal71 <uzzalroy.acm@gmail.com>
2019-01-15 21:51:32 +0600

    Create ViewSet for our profile feed item

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -119,4 +119,17 @@ class LoginViewSet(viewsets.ViewSet):
     def create(self, request):
         """Use the ObtainAuthToken APIView to validate and create a token."""
 
-        return ObtainAuthToken().post(request)                      
+        return ObtainAuthToken().post(request)
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items."""
+
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user."""
+
+        serializer.save(user_profile=self.request.user)                              
